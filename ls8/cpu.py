037bdfaad1890c9fb1e471fa847a01a540ad4487
nimu77 <nirmal.thapa77@outlook.com>
2020-10-13 08:58:58 -0700

    Day1-Ls8 Emulator

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,27 @@
 
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.reg[7] = 0xF4
+        self.pc = 0
+        self.halted = False    
+        
+    def ram_read(self, address):
+        '''accept the address to read and return the value stored there'''
+        return self.ram[address]
+
+    def ram_write(self, address, val):
+        '''accept a value to write, and the address to write it'''
+        self.ram[address] = val
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +77,20 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while not self.halted:
+            instruction = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if instruction == HLT:
+                self.halted = True
+                self.pc += 1
+
+            elif instruction == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif instruction == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+
